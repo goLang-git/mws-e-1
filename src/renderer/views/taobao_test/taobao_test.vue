bdcb3cb91cf3bb628f63e8ddae9a1c66b2d54ec3
goLang-git <4425997Gao>
2020-02-13 16:19:02 +0800

    添加淘宝

---
+++ src/renderer/views/taobao_test/taobao_test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <webview id="foo" src="https://www.taobao.com/" ></webview>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "taobao_test"
+    }
+</script>
+
+<style scoped>
+
+</style>
